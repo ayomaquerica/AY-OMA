988fb01fe785913e82f3b29a0cd10675326ed1d0
jgarcia <gsusli@hotmail.com>
2012-05-21 09:40:55 +0200

    segundo Commit...lo hago sobre la rama master

--- AY OMA/src/pozi.java
+++ AY OMA/src/pozi.java
@@ -8,6 +8,8 @@ public class pozi {
 		// TODO Auto-generated method stub
 		
 		System.out.println("el empiece");
+		
+		System.out.println("voy a hacer un commit al master...and commander");
 
 	}
 
